8107c8fff5580f81dc36b105aa796cfdebe090ae
justinmitchelltech <justin0mitchell@gmail.com>
2021-03-24 11:46:36 -0400

    Work through lunch 3/24

--- Python/plotting.py
+++ Python/plotting.py
@@ -1,21 +1,61 @@
+# ---------------------------------------------------------------------------------------
+#   Basic 2-D Line Plot
+# ---------------------------------------------------------------------------------------
+def plot_basic_2d(axhndl, x, y, tag='', descrip='', lnwidth=3, title='',
+                  xlabel='', ylabel='', fontsize=16):
+
+    # axhndl: axis handle
+    # x: x data
+    # y: y data
+    # tag: label of data set - primarily used for creating a legend
+    # descrip: color and line type
+    # lnwidth: line width
+    # title: plot title
+    # xlabel: x axis label
+    # ylabel: y axis label
+    # fontsize: font size of xlabel, ylabel, and title
+
+    axhndl.plot(x, y, descrip, label=tag, linewidth=lnwidth)
+    axhndl.set_xlabel(xlabel, fontsize=fontsize)
+    axhndl.set_ylabel(ylabel, fontsize=fontsize)
+    axhndl.set_title(title, fontsize=fontsize)
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 
 
-def plot(handle, x, y, \
-         descrip = '', \
-         title = '', xlabel = '', ylabel = '', \
-         fontsize = 16, label=''):
+# ---------------------------------------------------------------------------------------
+#   Basic 3-D Contour Plot
+# ---------------------------------------------------------------------------------------
+def plot_contour(axhndl, x1, x2, func, fill=False, title='', xlabel='', ylabel='',
+                 fontsize=16, colmap='bwr', cntrnum=15, lnlabels=False):
 
-    # https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+    # axhndl: axis handle
+    # x1: independent variable #1
+    # x2: independent variable #2
+    # func: function handle of function to plot
+    # fill: fill contour or just lines
+    # title: plot title
+    # xlabel: x axis label
+    # ylabel: y axis label
+    # fontsize: font size of xlabel, ylabel, and title
+    # colmap: colormap to use for contour plot
+    #   -> https://matplotlib.org/stable/tutorials/colors/colormaps.html
+    # cntrnum: number of contours
+    # lnlabels: whether or not to label contour lines
 
-
-    '''
-    Figure size (figsize) determines the size of the figure in inches.
-    Dots per inches (dpi) determines how many pixels the figure comprises.
-        - The default dpi in matplotlib is 100.
-    '''
-
-    handle.plot(x, y, descrip, label=label, linewidth=3)
-    handle.set_xlabel(xlabel, fontsize=fontsize)
-    handle.set_ylabel(ylabel, fontsize=fontsize)
-
-    # plt.show()
+    import numpy as np  # necessary to run nxt ln
+    xx1, xx2 = np.meshgrid(x1, x2)
+    yy = func([xx1, xx2])
+    if fill:
+        cs = axhndl.contourf(xx1, xx2, yy, cntrnum, cmap=colmap)
+        if lnlabels:
+            axhndl.clabel(cs, inline=1, fontsize=8)
+    else:
+        cs = axhndl.contour(xx1, xx2, yy, cntrnum, cmap=colmap)
+        if lnlabels:
+            axhndl.clabel(cs, inline=1, fontsize=8)
+    axhndl.set_xlabel(xlabel, fontsize=fontsize)
+    axhndl.set_ylabel(ylabel, fontsize=fontsize)
+    axhndl.set_title(title, fontsize=fontsize)
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
---
+++ Python/runner_plotting.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from plotting import *
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------
+#    How to make a basic figure using plotting library
+# ---------------------------------------------------------------------------------------
+
+# 1. Plot Setup
+plt.style.use('dark_background')
+fig, ax = plt.subplots(2, 2, figsize=(12, 8), dpi=200)
+'''
+- Subplots:
+     - If you just one one plot: plt.subplots(figsize=(12, 8), dpi=200)
+     - If you have them stacked, ax is a 1-D array
+     - If you have a grid of plots, ax is a 2-D array
+- Plot Styles:
+    https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+- Figure size (figsize) determines the size of the figure in inches.
+- Dots per inches (dpi) determines how many pixels the figure comprises.
+    - The default dpi in matplotlib is 100.
+'''
+
+# 2. Plotting
+plot_basic_2d(ax[0, 0], np.linspace(-10, 10, 200), np.sin(np.linspace(-10, 10, 200)),
+              title='Title or LaTeX: $\\pi^{xx}=2$', xlabel='x-axis', ylabel='y-axis',
+              tag='first one')
+
+plot_basic_2d(ax[1, 1], np.linspace(-10, 10, 200), np.cos(np.linspace(-10, 10, 200)),
+              title='Title', xlabel='x-axis', ylabel='y-axis',
+              descrip='g-.', tag='second one')
+def func3d(x):
+    return x[0]**2 + x[1]**2
+plot_contour(ax[0, 1], np.linspace(-10, 10, 200), np.linspace(-10, 10, 200), func3d,
+             lnlabels=False, fill=True)
+
+# 3. Plot Labeling
+ax[0, 0].legend()
+ax[1, 1].legend()
+fig.suptitle('all the plots', fontsize=40)
+
+# 4. Show Plot
+plt.show()
+
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
