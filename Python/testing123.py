7c46e27009b7977a7acb25736919938c2d7bdfb9
justinmitchelltech <justin0mitchell@gmail.com>
2021-04-10 07:11:59 -0400

    Update of 4-10

--- Python/testing123.py
+++ Python/testing123.py
@@ -9,3 +9,17 @@ for i in range1:
 
 for letter in string1:
     print(letter, '\t', ord(letter))
+
+print('\n', string1.upper())
+
+print('\n', type(string1))
+
+str2 = 'Testing this out.'
+print(str2.replace('Testing', '123'))
+
+str3 = 'Testing out if you can find justin@wahoofitness.com from this string.'
+atpos = str3.find('@')
+nxtspace = str3.find(' ', atpos)
+print(str3[atpos+1:nxtspace])
+
+print('\n', '© Justin Mitchell 🤘 Spring 2021')
