645f95a179c1981403fbc59a3b2196566ce1ba95
justinmitchelltech <justin0mitchell@gmail.com>
2021-04-17 13:19:31 -0400

    Did more FCC.

---
+++ Python/Learning_Lists.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+friend = 'Carly'
+print(friend[1])
+
+friends = ['Carly', 'Paulo', [1, 2, 3]]
+print(friends[2])
+for thing in friends:
+    if type(thing) is str:
+        print('String')
+    elif type(thing) is list:
+        print('List of length '+str(len(thing)))
+
+for i in range(len(friends)):
+    print(str(i)+': '+str(friends[i]))
+
+for i in range(6+1):
+    # print(i)
+    continue
+
+for i in np.arange(0, 6+.05, 0.1):
+    # print(i)
+    continue
+
+print('\n')
+a = [1, 2]
+b = [4, 5]
+c = a+b
+print(c)
+print(c[-1])
+print(c[0:-1])  # <-- remember "up to but not including" !!
+print(dir(c))  # print all of the available methods associated with it
+print(4 in c)
---
+++ Python/Objects.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class doStuff:
+    def __init__(self, var1=0, var2=0, var3=0):
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+    x = np.array([1])
+    def addStuff(self, a):
+        self.x = np.append(self.x, a)
+
+# New class that inherits doStuff, then extends it by adding var4 to existing stuff
+class doMoreStuff(doStuff):
+    var4 = 'newestStuff'
+
+
+# test = doStuff()
+test = doStuff(1, 2, 3)
+test.addStuff(7)
+test.addStuff(9)
+print(test.x)
+print(type(test))
+print(dir(test))
+
+# test2 = doMoreStuff(5, 6, 7)
+test2 = doMoreStuff()
+print(test2.x)
